8be7d0346d09acaadcba0e1adedd52d06e8f0988
Dema Maksim <maksimdema@gmail.com>
2021-01-24 23:49:58 +0200

    Updates

---
+++ main.154351dc17085e30f2a7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{L1EO:function(n,e,t){},QfWi:function(n,e,t){"use strict";t.r(e);t("L1EO"),t("JBxO"),t("FdtR");var o=function(n){return new Promise((function(e){setInterval((function(){e(n)}),n)}))},i=function(n){return console.log("Resolved after "+n+"ms")};o(2e3).then(i),o(1e3).then(i),o(1500).then(i)}},[["QfWi",1,2]]]);
+//# sourceMappingURL=main.154351dc17085e30f2a7.js.map
